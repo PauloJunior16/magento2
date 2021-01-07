916bef834bb0cf371910db0036a3863813b4a362
Karen Avelar <karen.avelar@outlook.com>
2021-01-07 19:23:00 -0300

    Alterando o modulo de produtos

--- app/etc/config.php
+++ app/etc/config.php
@@ -308,6 +308,7 @@ return [
         'Klarna_Kp' => 1,
         'Klarna_Onsitemessaging' => 1,
         'Klarna_KpGraphQl' => 1,
+        'Koala_CreateSampleProduct' => 1,
         'PayPal_Braintree' => 1,
         'PayPal_BraintreeGraphQl' => 1,
         'Temando_ShippingRemover' => 1,
