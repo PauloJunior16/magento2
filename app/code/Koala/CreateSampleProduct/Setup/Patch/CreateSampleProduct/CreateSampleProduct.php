00874aa175346fcadaefa1c7f187a6037fbe56ca
Karen Avelar <karen.avelar@outlook.com>
2021-01-11 12:08:17 -0300

    Adicionando DataPatchnos produtos

--- app/code/Koala/CreateSampleProduct/Setup/Patch/CreateSampleProduct/CreateSampleProduct.php
+++ app/code/Koala/CreateSampleProduct/Setup/Patch/CreateSampleProduct/CreateSampleProduct.php
@@ -1,90 +1,81 @@
 <?php
+declare(strict_types=1);
 
-class CreateSampleProduct 
+namespace Koala\CreateSampleProduct\Setup\Patch\Data;
+
+use Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface;
+use Magento\Eav\Setup\EavSetup;
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+
+class CreateCategories implements DataPatchInterface
 {
+    /**
+     * ModuleDataSetupInterface
+     *
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * EavSetupFactory
+     *
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * CreateCategories constructor.
+     *
+     * @param ModuleDataSetupInterface  $moduleDataSetup
+     * @param EavSetupFactory           $eavSetupFactory
+     */
     public function __construct(
-        \Magento\Framework\App\Helper\Context $context,
-        \Magento\Catalog\Model\ResourceModel\Product\Collection $productCollection,
-        \Magento\Catalog\Model\ProductFactory $productFactory,
-        \Magento\Catalog\Model\Product $product,
-        \Magento\Catalog\Model\Product\Option $option,
-        \Magento\Checkout\Model\Cart $cart,
-        array $data = []
+        ModuleDataSetupInterface $moduleDataSetup,
+        EavSetupFactory $eavSetupFactory
     ) {
-        $this->_productCollection = $productCollection;
-        $this->_productFactory = $productFactory;
-        $this->_prod = $product;
-        $this->_option = $option;
-        $this->_cart = $cart;
-        parent::__construct($context);
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->eavSetupFactory = $eavSetupFactory;
     }
-    
-    public function createProduct($sku, $name, $weight, $price, $jsonMix, $jsonPost, $type = 'simple')
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
     {
-        $mixProduct = $this->_productFactory->create();
-    
-        $mixProduct->setSku('test');
-        $mixProduct->setName('Teste');
-        $mixProduct->setWeight(2);
-        $mixProduct->setPrice(487);
-        $mixProduct->setDescription('' . $jsonMix . ' ' . $jsonPost);
-    
-        $mixProduct->setAttributeSetId(4);
-        $mixProduct->setStatus(1);
-        $mixProduct->setVisibility(4);
-        $mixProduct->setTaxClassId(150);
-        $mixProduct->setTypeId(100);
-        $mixProduct->setWebsiteIds(array(1));
-    
-        $mixProduct->setStockData(
-            array(
-                'use_config_manage_stock' => 0,
-                'manage_stock' => 0,
-                'is_in_stock' => 1,
-                'qty' => 10
-            )
-        );
-    
-        $product = $this->productFactory->create();
-    
-        try{
-            $mixProduct->save();
-        } catch(Exception $e){
-            echo $e->getMessage();
-        }
-    
-        var_dump($mixProduct->getId()); die();
-    
+        /** @var EavSetup $eavSetup */
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+
+        $eavSetup->addAttribute('catalog_product', 'recommended', [
+            'type' => 'int',
+            'label' => 'Is Recommended',
+            'input' => 'select',
+            'source' => 'Magento\Eav\Model\Entity\Attribute\Source\Boolean',
+            'default' => 0,
+            'global' => ScopedAttributeInterface::SCOPE_STORE,
+            'visible' => true,
+            'used_in_product_listing' => true,
+            'user_defined' => true,
+            'required' => false,
+            'group' => 'General',
+            'sort_order' => 80,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
     }
 }
-
-
-// $options = array(
-//     array(
-//         "sort_order" => 1,
-//         "title" => "Teste 1",
-//         "price_type" => "fixed",
-//         "price" => "10",
-//         "type" => "field",
-//         "is_require" => 0
-//     ),
-//     array(
-//         "sort_order" => 2,
-//         "title" => "Teste 2",
-//         "price_type" => "fixed",
-//         "price" => "20",
-//         "type" => "field",
-//         "is_require" => 0
-//     )
-// );
-
-// foreach ($options as $arrayOption) {
-//     $product->setHasOptions(1);
-//     $product->getResource()->save($product);
-//     $option = $objectManager->create('\Magento\Catalog\Model\Product\Option')
-//         ->setProductId($product->getId())
-//         ->setStoreId($product->getStoreId())
-//         ->addData($arrayOption);
-//     $option->save();
-//     $product->addOption($option);
-// }
